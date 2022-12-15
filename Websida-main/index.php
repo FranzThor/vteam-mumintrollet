544cb78ede8d3f3aa5517bfabcab859071174fe4
Emil Svedrin <emil.svderin@gmail.com>
2022-12-15 12:01:34 +0100

    uppdaterad nav

---
+++ Websida-main/index.php
@@ -0,0 +1,46 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script defer src="script.js"></script>
+    <title>Mummin trollen</title>
+</head>
+<body>
+    <div class="container">
+
+        <!-- Navbar -->
+        <div class="nav_container">
+            <div class="nav_content">
+                <div class="logo">
+                    <h1>Placeholder</h1>
+                </div>
+            </div>
+            <div class="nav_content nav_btn_container">
+                <div class="nav_btn active">
+                    <p class="nav_btn_text">
+                        Home
+                    </p>
+                </div>
+                <div class="nav_btn">
+                    <p class="nav_btn_text">
+                        History
+                    </p>
+                </div>
+            </div>
+            <div class="nav_content profile_container">
+                <div class="profile">
+
+                </div>
+            </div>
+        </div>
+        <!-- Navbar End -->
+
+        
+       <div class="hero">
+        <!-- <button type="button">Login</button> -->
+            <a class="hero-buttom" href="#hejsan">Home</a>
+       </div>
+    </div>
+    <div class="map">
+        <div class="mapouter"><div class="gmap_canvas"><iframe width="1260" height="575" id="gmap_canvas" src="https://maps.google.com/maps?q=blekinge%20tekniska%20h%C3%B6gskola&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-to.org">123 movies</a><br><style>.mapouter{position:relative;text-align:right;height:575px;width:1260px;}</style><a href="https://www.embedgooglemap.net">google maps widget html</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:575px;width:1260px;}</style></div></div>
