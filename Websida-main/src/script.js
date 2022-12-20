b31ae68da83175fd09724f29d2f3cdebb3f7f194
Emil Svedrin <emil.svedrin@gmail.com>
2022-12-20 12:36:15 +0000

    optimized selection script for the nav buttons. Added comments

--- Websida-main/src/script.js
+++ Websida-main/src/script.js
@@ -4,33 +4,42 @@
 (function () {
     "use strict";
 
-    const container = document.getElementsByClassName("container");
-    const id = container[0].id;
+    /* *******************************************************************************
+    * This code is made to add the active class on the right nav button.
+    * If you add a button to the navbar you will need to add an id to the button
+    * that symbolize the page the button is linked to, then add the same id to
+    * the main container in the linked page to get the script to work on your
+    * new button.
+    ******************************************************************************* */
 
-    const btn_home = document.getElementById("btn_home");
-    const btn_history = document.getElementById("btn_history");
-    console.log(id, btn_home);
+    const container = document.getElementsByClassName("container");     // selects the container to the page
+    const id = container[0].id;                                         // the variable is given the containers id 
 
-    if (id == "home") {
-        if (!btn_home.className.includes(" active")) {
-            btn_home.className += " active";
+    const nav_buttons = document.getElementsByClassName("nav_btn");     // selects the navbar buttons
+
+    /* This loop cycles all the nav buttons to match the container id with the button id */
+    for (let i = 0; i < nav_buttons.length; i++) {
+
+        /*  */
+        if (id == nav_buttons[i].id) {
+
+            /* Selects the button if active is not a class on the button */
+            /* If the we just add the class it might be duplicated */
+            if (!nav_buttons[i].className.includes("active")) {
+                nav_buttons[i].className += " active";
+            }
+
+            /* This loop cycles the nav buttons to remove the active class on the buttons */
+            /* not matched with the container */
+            for (let y = 0; y < nav_buttons.length; y++) {
+                if (!nav_buttons[y].id == nav_buttons[i].id) {
+                    if (nav_buttons[y].className.includes("active")) {
+                        nav_buttons[y].className -= " active";
+                    }
+                }
+            }
         }
-        if (btn_history.className.includes(" active")) {
-            btn_history.className -= " active";
-        }
-    } else {
-        console.log("no");
     }
-
-    if (id == "history") {
-        if (!btn_history.className.includes(" active")) {
-            btn_history.className += " active";
-        }
-        if (btn_home.className.includes(" active")) {
-            btn_home.className -= " active";
-        }
-    } else {
-        console.log("no");
-    }
+    /* **************************************************************************** */
 
 }());
